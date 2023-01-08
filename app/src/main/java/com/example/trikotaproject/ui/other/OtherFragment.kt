a0148dd1894d5465f03ea55c42271aebb0738ccd
adilzhexenov <nescrypnoadil@gmail.com>
2023-01-09 05:29:56 +0600

    add social networks

--- app/src/main/java/com/example/trikotaproject/ui/other/OtherFragment.kt
+++ app/src/main/java/com/example/trikotaproject/ui/other/OtherFragment.kt
@@ -1,23 +1,23 @@
 package com.example.trikotaproject.ui.other
 
 import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.trikotaproject.R
 import com.example.trikotaproject.contract.navigator
+import com.example.trikotaproject.databinding.DialogSocialNetworksBinding
 import com.example.trikotaproject.databinding.FragmentOtherBinding
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import java.lang.System.exit
 
 class OtherFragment : Fragment() {
 
     private var _binding: FragmentOtherBinding? = null
-
+    private lateinit var dialogBinding: DialogSocialNetworksBinding
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -28,12 +28,33 @@ class OtherFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentOtherBinding.inflate(inflater, container, false)
+        dialogBinding = DialogSocialNetworksBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        dialogBinding.youtubeBtn.setOnClickListener {
+            openLink("https://www.youtube.com/", container!!.context)
+        }
+        dialogBinding.telegramBtn.setOnClickListener {
+            openLink("https://web.telegram.org/", container!!.context)
+        }
+        dialogBinding.facebookBtn.setOnClickListener {
+            openLink("https://www.facebook.com/index.php", container!!.context)
+        }
+        dialogBinding.instagramBtn.setOnClickListener {
+            openLink("https://www.instagram.com/", container!!.context)
+        }
+        dialogBinding.linkedinBtn.setOnClickListener {
+            openLink("https://kz.linkedin.com/", container!!.context)
+        }
+
         binding.myProfileBtn.setOnClickListener {
             navigator().showMyProfile()
         }
         binding.logoutBtn.setOnClickListener {
             navigator().logOut()
+        }
+        binding.socialNetworksBtn.setOnClickListener {
+            alertShowSocialNetworks()
         }
         return root
     }
@@ -43,10 +64,21 @@ class OtherFragment : Fragment() {
         _binding = null
     }
 
+    private fun openLink(link: String, context: Context) {
+        val queryUrl:Uri = Uri.parse(link)
+        val intent = Intent(Intent.ACTION_VIEW, queryUrl)
+        context.startActivity(intent)
+    }
+
     private fun alertShowSocialNetworks(){
         val dialog = AlertDialog.Builder(context)
-            .setCancelable(true)
+            .setCancelable(false)
             .setTitle(R.string.social_networks)
-
+            .setView(dialogBinding.root)
+            .setNegativeButton("Close") {_, _ ->
+                navigator().showOtherPage()
+            }
+            .create()
+        dialog.show()
     }
 }
